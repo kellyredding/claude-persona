ce667a1823d401eb2c46906877e07e2738a94100
Kelly Redding <kelly@kellyredding.com>
2026-02-16 07:59:37 -0600

    Release 1.2.2

--- src/claude_persona.cr
+++ src/claude_persona.cr
@@ -1,7 +1,7 @@
 require "./claude_persona/*"
 
 module ClaudePersona
-  VERSION = "1.2.1"
+  VERSION = "1.2.2"
 
   # Allow override via environment variable for testing
   CONFIG_DIR   = Path.new(ENV.fetch("CLAUDE_PERSONA_CONFIG_DIR", (Path.home / ".claude-persona").to_s))
